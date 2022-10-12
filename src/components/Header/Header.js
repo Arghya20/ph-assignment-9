095c30605fcdda05d4cf7bdb9d7749d5173d597e
Arghya20 <arghyaghosh.vlog@gmail.com>
2022-10-12 23:21:09 +0530

    bug fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,8 +14,6 @@ const Header = () => {
             title="Company"
             className="inline-flex items-center"
           >
-            
-
             <div className="w-2/6">
               <img
                 src="https://storage.googleapis.com/rush-web-assets/images/home/quizzy_logo.png"
@@ -95,24 +93,12 @@ const Header = () => {
                         title="Company"
                         className="inline-flex items-center"
                       >
-                        <svg
-                          className="w-8 text-deep-purple-accent-400"
-                          viewBox="0 0 24 24"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                          strokeMiterlimit="10"
-                          stroke="currentColor"
-                          fill="none"
-                        >
-                          <rect x="3" y="1" width="7" height="12" />
-                          <rect x="3" y="17" width="7" height="6" />
-                          <rect x="14" y="1" width="7" height="6" />
-                          <rect x="14" y="11" width="7" height="12" />
-                        </svg>
-                        <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
-                          Company nam
-                        </span>
+                        <div className="w-2/6">
+                          <img
+                            src="https://storage.googleapis.com/rush-web-assets/images/home/quizzy_logo.png"
+                            alt=""
+                          />
+                        </div>
                       </Link>
                     </div>
                     <div>
